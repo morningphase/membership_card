3ca996a77f0ebe5a4c3952b30d43f5fc0c97bfdd
morningphase <49161035+morningphase@users.noreply.github.com>
2020-04-02 19:28:13 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:membership_card/pages/add_cards_with_camera.dart';
 import 'package:membership_card/pages/all_cards.dart';
 import 'package:membership_card/model/card_count.dart';
+import 'package:membership_card/pages/change_password.dart';
+import 'package:membership_card/pages/coupon.dart';
+import 'package:membership_card/pages/edit_card.dart';
 import 'package:membership_card/pages/help.dart';
+import 'package:membership_card/pages/login.dart';
+import 'package:membership_card/pages/register.dart';
 import 'package:membership_card/pages/search.dart';
 import 'package:membership_card/pages/settings.dart';
 import 'package:provider/provider.dart';
-
+import 'pages/user_info.dart';
 import 'model/card_model.dart';
+import 'model/user_model.dart';
 import 'pages/add_cards_with_number.dart';
-import 'pages/card_info.dart';
-
-void main() => runApp(GoWallet());
+import 'pages/forget_password.dart';
+import 'pages/card_info_membership.dart';
+import 'pages/card_info_barcode.dart';
+import 'pages/card_info_qrcode.dart';
+import 'package:membership_card/pages/bottom_menu.dart';
+import 'package:membership_card/pages/find_password.dart';
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(GoWallet());
+  });
+}
 
 /// This is the entrance of our GoWallet App.
 /// It is used for entering the whole GoWallet App and
@@ -22,32 +39,51 @@ void main() => runApp(GoWallet());
 class GoWallet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
     return MultiProvider(
 
-      // This is the providers for out state management
-      providers: [
-        ChangeNotifierProvider(
-          builder: (context) => CardCounter()
-        ),
-        ChangeNotifierProvider(
-          builder: (context) => CardInfo(),
-        )
-      ],
-      child: MaterialApp(
-        title: 'GoWallet App',
-        home: AllCardsPage(),
-        theme: ThemeData.light(),
-
-        // routes defined all the page routes of our App
-        routes: {
-          "/addnumber" : (_) => AddCardWithNumberPage(),
-          "/addcamera" : (_) => AddCardWithCameraPage(),
-          "/cardinfo"  : (_) => CardInfoPage(),
-          "/settings"  : (_) => SettingsPage(),
-          "/search"    : (_) => SearchPage(),
-          "/help"      : (_) => HelpPage(),
-        },
-      )
-    );
+        // This is the providers for out state management
+        providers: [
+          ChangeNotifierProvider(builder: (_) => CardCounter()),
+          ChangeNotifierProvider(
+            builder: (_) => CardInfo(),
+          ),
+          ChangeNotifierProvider(
+            builder: (_) => User(),
+          )
+        ],
+        child: MaterialApp(
+          title: 'GoWallet App',
+          theme: ThemeData(
+            primaryColor: Color.fromARGB(0xFF, 251, 108, 0),
+          ),
+          home: LoginPage(),
+          // routes defined all the page routes of our App
+          routes: {
+            "/addnumber": (_) => AddCardWithNumberPage(),
+            "/addcamera": (_) => AddCardWithCameraPage(),
+            "/cardinfo_membership": (_) => CardInfo1Page(),
+            "/cardinfo_barcode": (_) => CardInfo2Page(),
+            "/cardinfo_qrcode": (_) => CardInfo3Page(),
+            "/settings": (_) => SettingsPage(),
+            "/search": (_) => SearchPage(),
+            "/help": (_) => HelpPage(),
+            "/allCardsMainPage": (_) => AllCardsMainPage(),
+            '/allCardsPage': (_) => AllCardsPage(),
+            "/loginPage": (_) => LoginPage(),
+            "/registerPage": (_) => RegisterPage(),
+            "/forgetPage": (_) => ForgetPasswordPage(),
+            "/couponpage": (_) => CouponPage(),
+            "/edit":(BuildContext context) {
+              dynamic args = ModalRoute.of(context).settings.arguments;
+              CardInfo card = Provider.of<CardCounter>(context,listen:false).getCard(args["card"]);
+              return EditCardPage(card.cardId, card.eName);
+            },
+            "/user": (_) => UserInfoPage(),
+            "/bottomMenu": (_) => BottomMenuPage(),
+            "/changePasswordPage": (_) =>ChangePasswordPage(),
+            "/findpasswordpage":(_)=>FindPasswordPage(),
+          },
+        ));
   }
 }
